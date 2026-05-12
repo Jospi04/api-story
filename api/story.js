55c2aee8b219eaf1854e9062f38eaa3d4fc956c1
Joseph <djjhotamix@gmail.com>
2026-05-12 15:17:29 -0500

    api funcionando

--- api/story.js
+++ api/story.js
@@ -1,8 +1,81 @@
-export default async function handler(req, res) {
+import { io } from "socket.io-client";
+  const tokenRes = await fetch(
+    "https://anonsaver.com/connect/",
+    {
+      headers: {
+        Cookie: cookies,
+        Referer: `https://anonsaver.com/viewer/${username}/`,
+        "User-Agent": "Mozilla/5.0",
+      }
+    }
+  );
 
-    return res.status(200).json({
-        status: true,
-        message: "API funcionando"
+  const { token } = await tokenRes.json();
+
+  return new Promise((resolve, reject) => {
+
+    const socket = io("https://anonsaver.com", {
+      reconnection: false,
+      transports: ["polling", "websocket"],
+      extraHeaders: {
+        Cookie: cookies,
+        Referer: `https://anonsaver.com/viewer/${username}/`,
+        "User-Agent": "Mozilla/5.0",
+      }
     });
 
+    socket.on("connect", () => {
+
+      socket.emit("search", {
+        date: Date.now(),
+        token,
+        requestType: "1",
+        username,
+      });
+
+    });
+
+    socket.on("searchResult", (data) => {
+      socket.disconnect();
+      resolve(data);
+    });
+
+    socket.on("connect_error", reject);
+
+    setTimeout(() => {
+      reject(new Error("Timeout"));
+    }, 15000);
+
+  });
+
+
+export default async function handler(req, res) {
+
+  try {
+
+    const { user } = req.query;
+
+    if (!user) {
+      return res.status(400).json({
+        status: false,
+        message: "Falta el parámetro user"
+      });
+    }
+
+    const data = await getStories(user);
+
+    return res.status(200).json({
+      status: true,
+      creator: "Jospi",
+      result: data
+    });
+
+  } catch (err) {
+
+    return res.status(500).json({
+      status: false,
+      error: err.message
+    });
+
+  }
 }
